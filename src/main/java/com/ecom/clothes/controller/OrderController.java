c13eba381d1ff54736fa617c576faff57339f018
narin-rouen <figfdhb@gmail.com>
2026-04-26 11:48:17 +0200

    Created Order Controller and Built Place order endpoint for user

---
+++ src/main/java/com/ecom/clothes/controller/OrderController.java
@@ -0,0 +1,29 @@
+package com.ecom.clothes.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ecom.clothes.config.SecurityUser;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class OrderController {
+
+	private final OrderService orderService;
+
+	@PostMapping("/api/user/orders")
+	@PreAuthorize("hasAuthority('USER')")
+	public ResponseEntity<OrderResponse> placeOrder(@AuthenticationPrincipal SecurityUser securityUser) {
+		Long userId = securityUser.getUser().getId();
+		log.info("Received request to place order for userId: {}", userId);
+		OrderResponse response = orderService.placeOrder(userId);
+		return ResponseEntity.ok(response);
+	}
+}
