a58c54b4ec09a42833da5f6fc774108e0720d960
narin-rouen <figfdhb@gmail.com>
2026-05-01 12:44:05 +0200

    Built 2 endpoints for admin update order status + user confirm receive order

--- src/main/java/com/ecom/clothes/controller/OrderController.java
+++ src/main/java/com/ecom/clothes/controller/OrderController.java
@@ -3,7 +3,9 @@ package com.ecom.clothes.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,6 +32,24 @@ public class OrderController {
 		Long userId = securityUser.getUser().getId();
 		log.info("Received request to place order for userId: {}", userId);
 		OrderResponse response = orderService.placeOrder(userId, paymentRequest);
+		return ResponseEntity.ok(response);
+	}
+
+	@PutMapping("/api/admin/order/{id}")
+	@PreAuthorize("hasAuthority('ADMIN')")
+	public ResponseEntity<OrderResponse> updateOrderStatus(@PathVariable Long orderId){
+		log.info("Admin update status order with id: {}", orderId);
+		OrderResponse response = orderService.updateStatus(orderId);
+		return ResponseEntity.ok(response);>
+	}
+
+	@PostMapping("/api/user/order/{id}")
+	@PreAuthorize("hasAuthority('USER')")
+	public ResponseEntity<OrderResponse> confirmReceive(@AuthenticationPrincipal SecurityUser securityUser,
+			@PathVariable Long orderId) {
+		Long userId = securityUser.getUser().getId();
+		log.info("User confirms revieve order with id: {}", orderId);
+		OrderResponse response = orderService.confirmReceive(userId, orderId);
 		return ResponseEntity.ok(response);
 	}
 }
